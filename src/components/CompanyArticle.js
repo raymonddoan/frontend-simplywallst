35d3eaf6de6a7508db63b40874d3b87387975daa
whywesmurfing <raymond.doan94@gmail.com>
2021-07-25 23:43:51 +1000

    Added styled-components pkg and styling to each component

--- src/components/CompanyArticle.js
+++ src/components/CompanyArticle.js
@@ -1,4 +1,58 @@
 import React from 'react';
+import styled from "styled-components";
+
+const StyledCompanyArticle = styled.article`
+  border: solid 0.2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 1rem;
+`
+
+const StyledTextDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-grow: 1;
+  justify-content: space-evenly;
+  width: 240px;
+  padding: 1rem 0;
+`
+
+const StyledLeftDiv = styled.div`
+  color: ${props => props.color === "stronger" ? "#6ff776" : "#f76f6f"};
+
+  display: flex;
+  flex-direction: column;
+  width: 100px;
+`
+
+const StyledScore = styled.p`
+  margin: 0.2rem 0;
+`
+
+const StyledRightDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 140px;
+`
+
+const StyledTitleDiv = styled.div`
+  width: 100%;
+  overflow: hidden;
+`
+
+const StyledTitle = styled.h1`
+  font-size: 1.3rem;
+  font-weight: 800;
+  margin: 0.5rem 0;
+  width: max-content;
+`
+
+const StyledSymbol = styled.p`
+  margin: 0.1rem;
+  width: max-content;
+`
+
 
 const CompanyArticle = ({company}) => {
   const {name, unique_symbol, score} = company
@@ -9,13 +63,20 @@ const CompanyArticle = ({company}) => {
 
   return (
     <>
-      <article>
-        <div>
-          <h1>{name}</h1>
-          <h3>{unique_symbol}</h3>
-          <p>Overall Snowflake Score: {score}</p>
-        </div>
-      </article>
+      <StyledCompanyArticle>
+        <StyledTextDiv>
+          <StyledLeftDiv color={score < 12 ? null : "stronger"}>
+            <StyledScore>Overall Snowflake Score:</StyledScore>
+            <StyledScore>{score}</StyledScore>
+          </StyledLeftDiv>
+          <StyledRightDiv>
+            <StyledTitleDiv>
+              <StyledTitle>{name}</StyledTitle>
+            </StyledTitleDiv>
+            <StyledSymbol>{unique_symbol}</StyledSymbol>
+          </StyledRightDiv>
+        </StyledTextDiv>
+      </StyledCompanyArticle>
     </>
   )
 }
