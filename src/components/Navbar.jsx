f83ce3865f81de0784aa2d03e25b906b25b991bf
whywesmurfing <raymond.doan94@gmail.com>
2021-07-26 20:02:24 +1000

    Updated design of components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,36 @@ import React from 'react';
 import DropdownFilter from './DropdownFilter';
 import styled from "styled-components";
 
+const StyledNavbar = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 0 2rem;
+  height: 2rem;
+  background-color: #267491;
+`
+
+const StyledSortDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 30%;
+`
+
+const StyledButtonDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 40%;
+`
+
 const StyledFilterDiv = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-around;
   align-items: center;
+  width: 40%;
 `
 
 const NavBar = (props) => {
@@ -29,18 +54,20 @@ const NavBar = (props) => {
   scores.unshift("")
 
   return (
-    <>
-      <div>
+    <StyledNavbar>
+      <StyledSortDiv>
         <p>Sort by:</p>
-        <button onClick={() => setSortedField("score")}>Score</button>
-        <button onClick={() => setSortedField("volatility")}>Volatility</button>
-      </div>
+        <StyledButtonDiv>
+          <button onClick={() => setSortedField("score")}>Score</button>
+          <button onClick={() => setSortedField("volatility")}>Volatility</button>
+        </StyledButtonDiv>
+      </StyledSortDiv>
       <StyledFilterDiv>
         <p>Filter by: </p>
         <DropdownFilter data={exchanges} setValue={setExchange} label="Exchange"/>
         <DropdownFilter data={scores} setValue={setScore} label="Scores"/>
       </StyledFilterDiv>
-    </>
+    </StyledNavbar>
   )
 }
 
